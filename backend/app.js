36e3794da14f4235f3b495ea7a1fcd894b7f8b75
Adam Hermansson <qurridutt@gmail.com>
2018-03-09 12:11:18 +0100

    Man blev nu verkligen kvinna

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const { Client } = require('pg');
 var app = express()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('tjena mannen')
+  res.send('tjena kvinnan')
 });
 
 

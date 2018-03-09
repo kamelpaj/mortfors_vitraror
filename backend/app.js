e690e6a9b8af1ced8a8556ccb2601aa673dd3459
Adam Hermansson <qurridutt@gmail.com>
2018-03-09 17:31:10 +0100

    Added some queries to the database

--- backend/app.js
+++ backend/app.js
@@ -21,13 +21,36 @@ app.get('/', function (req, res) {
 app.get('/get_articles', async function (req, res) {
   const {rows} = await client.query('select distinct ProduktLagerSaldoPrisInformation.produktid, ProduktLagerSaldoPrisInformation.saldo, ProduktLagerSaldoPrisInformation.säljstyckpris, ProduktInköpsInformation.tillverkare from produktlagersaldoprisinformation inner join produktinköpsinformation on produktlagersaldoprisinformation.produktid=produktinköpsinformation.produktid;')
   console.log(rows);
-
   res.send(rows);
 });
 
-// app.post('/new_order', async function (req, res) {
-//   const {  }
-// });
+// Customer registration and ...
+app.get('/new_order/:id', async function (req, res) {
+  var id = req.params.id;
+
+  // Insert into Kundinfo
+  await client.query('insert into Kundinfo values (910102, Kuk Hedlund, Adress, Epost)')
+
+  // Insert into Orders
+  await client.query('insert into Orders values (OrderID, Datum, Personnummer)')
+
+  // Insert into Kvitto
+  await client.query('insert into Kvitto values (OrderID, ProduktID, SäljStyckPris, Antal)')
+
+  // Transaction between Kvitto and ProduktLagerSaldoPrisInformation
+  await client.query('begin;')
+  await client.query('update kvitto set antal = antal + 'FIXA' where orderid='FIXA';')
+  await client.query('update produktlagersaldoprisinformation set saldo = saldo - 'FIXA' where produktid='FIXA';')
+  await client.query('commit;')
+
+});
+
+
+app.get('/supplier_info', async function (req, res) {
+  const {rows} = await client.query('select * from LeverantörsInformation')
+  console.log(rows);
+  res.send(rows);
+});
 
 
 app.listen(3000, function(){

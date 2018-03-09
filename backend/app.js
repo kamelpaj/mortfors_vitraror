094e356530a655b33c71b3d9a18f891cd9fbd5de
Adam Hermansson <qurridutt@gmail.com>
2018-03-09 13:20:23 +0100

    Now fetches articles from database

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const { Client } = require('pg');
 
+const client = new Client({
+  user: 'ah7352',
+  host: 'pgserver.mah.se',
+  database: 'mortforsvitvaror',
+  password: 'n52c5oux',
+  port: 5432,
+});
+client.connect();
+
+
 var app = express()
-// respond with "hello world" when a GET request is made to the homepage
+// Landing route for application
 app.get('/', function (req, res) {
   res.send('tjena kvinnan')
 });
+
+// Fetches articles from database
+app.get('/get_articles', async function (req, res) {
+  const {articles} = await client.query('select distinct ProduktLagerSaldoPrisInformation.produktid, ProduktLagerSaldoPrisInformation.saldo, ProduktLagerSaldoPrisInformation.säljstyckpris, ProduktInköpsInformation.tillverkare from produktlagersaldoprisinformation inner join produktinköpsinformation on produktlagersaldoprisinformation.produktid=produktinköpsinformation.produktid;')
+  console.log(articles);
+  res.send(articles);
+});
+
 
 
 app.listen(3000, function(){
   console.log("Serverbrush mkt fet bomshakalak!");
 });
 
-const client = new Client({
-  user: 'ah7352',
-  host: 'pgserver.mah.se',
-  database: 'bizznizz',
-  password: 'n52c5oux',
-  port: 5432,
-});
-client.connect();
 
-client.query('select * from anstalld;', function(err, res) {
-  console.log(err, res);
-  client.end();
-});
+// client.query('select * from anstalld;', function(err, res) {
+//   console.log(err, res);
+//   client.end();
+// });
 
 // const text = 'SELECT * from PROJEKT';
 // window.text = text;
